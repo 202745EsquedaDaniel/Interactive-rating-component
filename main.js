c0807a49232e8daf4539eca3ff11f20febe2f135
00lar <eduardani84@gmail.com>
2023-03-09 01:36:32 -0700

    page finished

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const rate4 = document.querySelector("#btnfeed4");
 const rate5 = document.querySelector("#btnfeed5");
 const btnsubmit = document.querySelector("#submitbtn");
 const Pestaña_gracias = document.querySelector("#Pestaña_gracias");
+const selectednuevo = document.querySelector("#selected");
+let selected = 0;
 
 rate.addEventListener("click", togglebtnfeed);
 rate2.addEventListener("click", togglebtnfeed2);
@@ -20,6 +22,8 @@ function togglebtnfeed() {
   rate3.classList.remove("btnactive");
   rate4.classList.remove("btnactive");
   rate5.classList.remove("btnactive");
+  selected = 1;
+  updateSelected();
 }
 
 function togglebtnfeed2() {
@@ -29,6 +33,8 @@ function togglebtnfeed2() {
   rate3.classList.remove("btnactive");
   rate4.classList.remove("btnactive");
   rate5.classList.remove("btnactive");
+  selected = 2;
+  updateSelected();
 }
 
 function togglebtnfeed3() {
@@ -38,6 +44,8 @@ function togglebtnfeed3() {
   rate2.classList.remove("btnactive");
   rate4.classList.remove("btnactive");
   rate5.classList.remove("btnactive");
+  selected = 3;
+  updateSelected();
 }
 
 function togglebtnfeed4() {
@@ -47,17 +55,31 @@ function togglebtnfeed4() {
   rate2.classList.remove("btnactive");
   rate3.classList.remove("btnactive");
   rate5.classList.remove("btnactive");
+  selected = 4;
+  updateSelected();
 }
 
 function togglebtnfeed5() {
   rate5.classList.toggle("btnactive");
-  console.log("hola");
+  
   rate.classList.remove("btnactive");
   rate2.classList.remove("btnactive");
   rate3.classList.remove("btnactive");
   rate4.classList.remove("btnactive");
+  selected = 5;
+  console.log(selected);
+  updateSelected();
 }
 
 function removeHidden() {
   Pestaña_gracias.classList.remove("ocultar");
+}
+
+function updateSelected() {
+  selectednuevo.innerHTML = "you selected "+ selected + " out of 5";
+  btnsubmit.disabled = false;
+  
+}
+if (selected == 0) {
+  btnsubmit.disabled = true;
 }
