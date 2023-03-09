cbb3fe317cd419272b9f83bf54560a8a5fae42ec
00lartks <eduardani84@gmail.com>
2023-03-09 00:29:26 -0700

    implementacion de hidden

--- main.js
+++ main.js
@@ -3,55 +3,61 @@ const rate2 = document.querySelector("#btnfeed2");
 const rate3 = document.querySelector("#btnfeed3");
 const rate4 = document.querySelector("#btnfeed4");
 const rate5 = document.querySelector("#btnfeed5");
+const btnsubmit = document.querySelector("#submitbtn");
+const Pestaña_gracias = document.querySelector("#Pestaña_gracias");
 
-rate.addEventListener("click",togglebtnfeed)
+rate.addEventListener("click", togglebtnfeed);
+rate2.addEventListener("click", togglebtnfeed2);
+rate3.addEventListener("click", togglebtnfeed3);
+rate4.addEventListener("click", togglebtnfeed4);
+rate5.addEventListener("click", togglebtnfeed5);
+btnsubmit.addEventListener("click", removeHidden);
 
 function togglebtnfeed() {
-    rate.classList.toggle("btnactive");
-    console.log("hola")
-    rate2.classList.remove("btnactive")
-    rate3.classList.remove("btnactive")
-    rate4.classList.remove("btnactive")
-    rate5.classList.remove("btnactive")
+  rate.classList.toggle("btnactive");
+  console.log("hola");
+  rate2.classList.remove("btnactive");
+  rate3.classList.remove("btnactive");
+  rate4.classList.remove("btnactive");
+  rate5.classList.remove("btnactive");
 }
-rate2.addEventListener("click",togglebtnfeed2)
 
 function togglebtnfeed2() {
-    rate2.classList.toggle("btnactive");
-    console.log("hola")
-    rate.classList.remove("btnactive")
-    rate3.classList.remove("btnactive")
-    rate4.classList.remove("btnactive")
-    rate5.classList.remove("btnactive")
+  rate2.classList.toggle("btnactive");
+  console.log("hola");
+  rate.classList.remove("btnactive");
+  rate3.classList.remove("btnactive");
+  rate4.classList.remove("btnactive");
+  rate5.classList.remove("btnactive");
 }
-rate3.addEventListener("click",togglebtnfeed3)
 
 function togglebtnfeed3() {
-    rate3.classList.toggle("btnactive");
-    console.log("hola")
-    rate.classList.remove("btnactive")
-    rate2.classList.remove("btnactive")
-    rate4.classList.remove("btnactive")
-    rate5.classList.remove("btnactive")
+  rate3.classList.toggle("btnactive");
+  console.log("hola");
+  rate.classList.remove("btnactive");
+  rate2.classList.remove("btnactive");
+  rate4.classList.remove("btnactive");
+  rate5.classList.remove("btnactive");
 }
-rate4.addEventListener("click",togglebtnfeed4)
 
 function togglebtnfeed4() {
-    rate4.classList.toggle("btnactive");
-    console.log("hola")
-    rate.classList.remove("btnactive")
-    rate2.classList.remove("btnactive")
-    rate3.classList.remove("btnactive")
-    rate5.classList.remove("btnactive")
+  rate4.classList.toggle("btnactive");
+  console.log("hola");
+  rate.classList.remove("btnactive");
+  rate2.classList.remove("btnactive");
+  rate3.classList.remove("btnactive");
+  rate5.classList.remove("btnactive");
 }
 
-rate5.addEventListener("click",togglebtnfeed5)
-
 function togglebtnfeed5() {
-    rate5.classList.toggle("btnactive");
-    console.log("hola")
-    rate.classList.remove("btnactive")
-    rate2.classList.remove("btnactive")
-    rate3.classList.remove("btnactive")
-    rate4.classList.remove("btnactive")
+  rate5.classList.toggle("btnactive");
+  console.log("hola");
+  rate.classList.remove("btnactive");
+  rate2.classList.remove("btnactive");
+  rate3.classList.remove("btnactive");
+  rate4.classList.remove("btnactive");
+}
+
+function removeHidden() {
+  Pestaña_gracias.classList.remove("ocultar");
 }
